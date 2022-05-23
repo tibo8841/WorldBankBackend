bc55d2e263b04161d305af033f7f2bc9d09dbf08
tibo8841 <thibaut.hucker@gmail.com>
2022-05-23 15:46:28 +0100

    adding basic server file to see postgres stuff

---
+++ server.js
@@ -0,0 +1,17 @@
+import { Application } from "https://deno.land/x/abc/mod.ts";
+import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
+import { abcCors } from "https://deno.land/x/cors/mod.ts";
+
+const client = new Client(
+  "postgres://czreijar:TJ2StTuQIl2CoRoinQTwPxk8pBGfdf6t@kandula.db.elephantsql.com/czreijar"
+);
+
+await client.connect();
+
+const countryCodes = (
+  await client.queryObject("SELECT CountryCode FROM countries")
+).rows;
+
+console.log(countryCodes);
+
+const app = new Application();
