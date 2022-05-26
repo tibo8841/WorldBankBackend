04d450a0ed92930a547b3983641ff0ef0112f09e
tibo8841 <thibaut.hucker@gmail.com>
2022-05-26 14:37:21 +0100

    server ready to deploy

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import { Application } from "https://deno.land/x/abc/mod.ts";
 import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
 import { abcCors } from "https://deno.land/x/cors/mod.ts";
+import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-// url below is database being hosted on elephantSQL
+const DENO_ENV = Deno.env.get("DENO_ENV") ?? "development";
+config({ path: `./.env.${DENO_ENV}`, export: true });
 
-const clientUser = new Client(
-  `postgres://xtvjajky:1I2LmKlHwfx8RgKOYf6N-81Gg4eRUH_J@tyke.db.elephantsql.com/xtvjajky`
-);
+const clientUser = new Client(`Deno.env.get("PG_URL")`);
 await clientUser.connect();
 
 const clientCountry = new Client(
@@ -14,44 +15,25 @@ const clientCountry = new Client(
 );
 await clientCountry.connect();
 
-// BELOW IS EXAMPLE OF INSERT QUERY
-
-// await clientUser.queryArray(
-//   `INSERT INTO users (username, password, created_at) VALUES ('test', 'password', NOW());`
-// );
-
-// EXAMPLE BELOW OF HOW TO QUERY DATABASE TO SHOW WHATS IN USER TABLE AND CONSOLE LOG IT
-
-// let boop = await clientUser.queryObject(`SELECT * FROM users`);
-
-// console.log(boop);
-// console.log(boop.rows);
-
-// await clientUser.queryArray(
-//   `INSERT INTO search_history (user_id, countries, indicators, year_range, created_at) VALUES ('1', 'france', 'number of croissants', '1990-1999', NOW());`
-// );
-
-// let searchBloop = await clientUser.queryObject("SELECT * FROM search_history");
-// console.log(searchBloop.rows);
-
 const app = new Application();
+const PORT = Number(Deno.env.get("PORT"));
+
+const corsSettings = {
+  allowedHeaders: [
+    "Authorization",
+    "Content-Type",
+    "Accept",
+    "Origin",
+    "User-Agent",
+  ],
+  credentials: true,
+};
 
 app
-  .use(
-    abcCors({
-      allowedHeaders: [
-        "Authorization",
-        "Content-Type",
-        "Accept",
-        "Origin",
-        "User-Agent",
-      ],
-      credentials: true,
-    })
-  )
+  .use(abcCors(corsSettings))
   .get("/login", async (server) => {
     await getUserLogin(server);
-    await createSession(server);
+    // await createSession(server);
   })
   .post("/register", async (server) => {
     await registerNewUser(server);
@@ -64,4 +46,134 @@ app
   })
   .get("/history", async (server) => {
     await getUserHistory(server);
-  });
+  })
+  .start({ port: PORT });
+
+async function getUserLogin(server) {
+  const { username, password } = await server.body;
+
+  const query = await clientUser.queryObject(
+    `SELECT * FROM users WHERE username = $1;`,
+    username
+  );
+
+  if (query.rows.length) {
+    const passwordConfirm = await clientUser.queryObject(
+      `SELECT password FROM users WHERE username = $1;`,
+      username
+    );
+
+    const comparison = await bcrypt.compare(
+      password,
+      `${passwordConfirm.rows[0].password}`
+    );
+
+    if (comparison === true) {
+      return server.json({ response: "Success, you are now logged in" }, 200);
+    } else {
+      return server.json(
+        { error: "Incorrect password, please try again!" },
+        400
+      );
+    }
+  } else {
+    return server.json(
+      {
+        err: "An account with that username does not exist, please sign up",
+      },
+      400
+    );
+  }
+}
+
+import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
+
+export async function registerNewUser(server) {
+  const { username, password, passwordConfirmation } = await server.body;
+
+  if (username && password && passwordConfirmation) {
+    if (password === passwordConfirmation) {
+      const encryptedPassword = await bcrypt.hash(password);
+
+      try {
+        await clientUser.queryObject(
+          `INSERT INTO users (username, password, created_at) VALUES ($1, $2, NOW());`,
+          username,
+          encryptedPassword
+        );
+        return server.json({ response: "New account created!" }, 200);
+      } catch (err) {
+        return server.json(
+          {
+            err: "An account already exists for this username",
+          },
+          400
+        );
+      }
+    } else {
+      return server.json(
+        { error: "Passwords do not match, please try again" },
+        400
+      );
+    }
+  } else {
+    server.json({ error: "Please provide all data" }, 400);
+  }
+}
+
+async function getSearchResults(server) {
+  const { country, indicator, yearStart, yearEnd } = await server.body;
+
+  const searchResponse = await clientUser.queryObject(
+    `SELECT ShortName, IndicatorName, Value, Year, FROM Country 
+    JOIN Indicators ON Indicators.CountryCode = Country.CountryCode
+    WHERE ShortName = $1 AND IndicatorName = $2 AND Year <= $3 AND Year >= $4`,
+    country,
+    indicator,
+    yearStart,
+    yearEnd
+  );
+
+  const searchResult = searchResponse.rows;
+  await server.json(searchResult);
+  return server.json({ response: "Search Complete" }, 200);
+}
+
+async function postSearchHistory(server) {
+  const { userID, country, indicator, yearRange } = await server.body;
+
+  if (country && indicator && yearRange) {
+    await clientUser.queryObject(
+      `INSERT INTO search_history (user_id, countries, year_range, indicators,created_at) VALUES ($1, $2, $3, $4,NOW());`,
+      userID,
+      country,
+      yearRange,
+      indicator
+    );
+    return server.json({ response: "Added to database" }, 200);
+  } else if (countries && yearRange) {
+    await clientUser.queryObject(
+      `INSERT INTO search_history (user_id, countries, year_range,created_at) VALUES ($1, $2, $3, NOW());`,
+      userID,
+      country,
+      yearRange
+    );
+    return server.json({ response: "Added to database" }, 200);
+  } else {
+    return server.json({ response: "Missing necessary data" }, 404);
+  }
+}
+
+async function getUserHistory(server) {
+  const { userID } = await server.body;
+  const historyResult = await clientUser.queryObject(
+    `SELECT * FROM search_history WHERE user_id = $1`,
+    userID
+  );
+  if (historyResult) {
+    await server.json(historyResult);
+    return server.json({ response: "Here is search history" }, 200);
+  } else {
+    return server.json({ error: "ERROR" });
+  }
+}
