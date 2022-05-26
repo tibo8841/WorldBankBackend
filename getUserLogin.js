57e8cdc3b822fafe97cf6ef79f3274f2876394a0
aycin1 <aycinkaya98@gmail.com>
2022-05-26 14:23:39 +0100

    Completed function for logging user in

--- getUserLogin.js
+++ getUserLogin.js
@@ -2,28 +2,37 @@ import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 
 export async function getUserLogin(server) {
   const { username, password } = await server.body;
+
   const query = await clientUser.queryObject(
-    `SELECT * FROM users WHERE username = ${username};`
+    `SELECT * FROM users WHERE username = $1;`,
+    username
   );
-  if (query) {
+
+  if (query.rows.length) {
     const passwordConfirm = await clientUser.queryObject(
-      `SELECT password FROM users WHERE username = ${username};`
+      `SELECT password FROM users WHERE username = $1;`,
+      username
     );
 
-    const encryptedPassword = await bcrypt.hash(password);
-    const encryptedPasswordConfirm = await bcrypt.hash(passwordConfirm);
-
     const comparison = await bcrypt.compare(
-      encryptedPassword,
-      encryptedPasswordConfirm
+      password,
+      `${passwordConfirm.rows[0].password}`
     );
 
     if (comparison === true) {
       return server.json({ response: "Success, you are now logged in" }, 200);
+    } else {
+      return server.json(
+        { error: "Incorrect password, please try again!" },
+        400
+      );
     }
   } else {
-    return server.json({
-      error: "An account with that username does not exist, please sign up",
-    });
+    return server.json(
+      {
+        err: "An account with that username does not exist, please sign up",
+      },
+      400
+    );
   }
 }
